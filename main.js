eb8d502dcdbcfd34e5dace7da3a20be435886697
Xen-alpha <hky7548@naver.com>
2025-01-13 14:03:37 +0900

    Feat: implement stock price history api

--- main.js
+++ main.js
@@ -2,7 +2,14 @@ const fs = require("node:fs");
 const cors = require("cors");
 const express = require("express");
 const app = express({ strict: true });
-const port = 8080;
+const port = 8000;
+const mongoose = require("mongoose");
+
+const connection = mongoose.connect("mongodb://127.0.0.1:27017/americastock");
+
+// DB 스키마 생성하고 모델화
+const schema = mongoose.Schema({name: String, code: {type: String, required:true} , price: {type: String, required:true}, date: {type: String, required:true}}, {collection:"stock"});
+const stockPrice = mongoose.model("StockPrice", schema);
 
 app.use(
   cors({
@@ -10,6 +17,8 @@ app.use(
     credentials: false,
   })
 );
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.status(404).send({ isSuccess: false, reason: "Not a valid URI path" });
@@ -31,9 +40,21 @@ app.get("/stocklist", (req, res) => {
     const amexArray = JSON.parse(fs.readFileSync(amex[0], { encoding: "utf-8" })).data.rows.map((value) => value.symbol); // read-only
     res.send({ isSuccess: true, result: nasdaqArray.concat(nyseArray, amexArray).sort() });
   } catch (e) {
-    res.sendStatus(503).send({ isSuccess: false, reason: "Failed to read server-side data", requestBody: req.body });
+    res.status(503).send({ isSuccess: false, reason: "Failed to read server-side data", requestBody: req.body });
     return;
   }
+});
+
+app.get("/stock", async (req,res) => {
+  try {
+    const ticker = req.body.code;
+
+    const query = await stockPrice.find({code: ticker});
+    res.send({isSuccess: true, code: ticker, result: query});
+  } catch (e) {
+    res.status(400).send("Bad Request: " + e);
+  }
+  
 });
 
 app.get("/stockinfotest", (req, res) => {
@@ -47,3 +68,11 @@ app.get("/stockinfotest", (req, res) => {
 app.listen(port, () => {
   console.log(`server opened in ${port}`);
 });
+
+app.on("error", () => {
+  mongoose.disconnect();
+})
+
+app.on("close", () => {
+  mongoose.disconnect();
+})
