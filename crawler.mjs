bc06d458c9035ffde8aeb9b1b2db3179ad1b3e70
Xen-alpha <hky7548@naver.com>
2025-02-28 16:54:11 +0900

    [Fix] fix synchronization problem

--- crawler.mjs
+++ crawler.mjs
@@ -14,9 +14,13 @@ let timerInstance;
 const callback = async (totalStocks, stockPrice, key) => {
   console.log(`Fetching ${totalStocks[counter].code} Data...`);
   let olddate = new Date(Date.now());
+  let code = totalstocks[counter].code;
+  let market= totalstocks[counter].market;
+  let name = totalstocks[counter].name;
+  counter++;
   olddate.setMonth(olddate.getMonth() - 23);
   // 최근 1년 데이터 받아옴
-  let response = await fetch(`https://api.polygon.io/v2/aggs/ticker/${totalStocks[counter].code}/range/1/day/${getDateFormat(olddate)}/${getDateFormat(new Date(Date.now()))}?adjusted=true&sort=desc&apiKey=${key}`);
+  let response = await fetch(`https://api.polygon.io/v2/aggs/ticker/${code}/range/1/day/${getDateFormat(olddate)}/${getDateFormat(new Date(Date.now()))}?adjusted=true&sort=desc&apiKey=${key}`);
   response = await response.json();
   const timeSeries = response.results.map((value) => {
     let newObject = {};
@@ -25,19 +29,18 @@ const callback = async (totalStocks, stockPrice, key) => {
     return newObject;
   });
   for (const item of timeSeries) {
-    const result = await stockPrice.findOne({ code: totalStocks[counter].code, date: item.timestamp});
+    const result = await stockPrice.findOne({ code: code, date: item.timestamp});
     if (result) {
       console.log(`Date ${item.timestamp} already exists for ${totalStocks[counter].code}`);
       continue;
     }
-    const instance = new stockPrice({id: counter, name: totalStocks[counter].name, code: totalStocks[counter].code, market: totalStocks[counter].market, price: item.price, date: item.timestamp });
+    const instance = new stockPrice({id: counter, name: name, code: code, market: market, price: item.price, date: item.timestamp });
     try {
       await instance.save();
     } catch (e) {
         console.error(e);
     }
   }
-  counter++;
   console.log(`Fetched ${totalStocks[counter].code} Data...`);
   if (counter >= totalStocks.length) {
     mongoose.disconnect();
